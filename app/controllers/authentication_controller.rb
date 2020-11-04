a068766ef700ed339c0aa9b77440ccd2e1895038
LimitToInfinity <21stLimitless@gmail.com>
2020-11-04 15:55:38 -0700

    Adds catches for bad username and bad password in login action. Adds jwt gem.

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,6 +1,23 @@
 class AuthenticationController < ApplicationController
 
   def login
+    @user = User.find_by username: params[:username]
+
+    if !@user
+      render json: { message: 'Incorrect username' },
+        status: :unauthorized
+    else
+
+      if !@user.authenticate(params[:password])
+        render json: { message: 'Incorrect password' },
+          status: :unauthorized
+      else
+
+        
+
+      end
+
+    end
   end
 
 end
