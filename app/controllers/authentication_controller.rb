c72f293a79e958e31e37403188bb5101f4389eb7
LimitToInfinity <21stLimitless@gmail.com>
2020-11-04 16:10:54 -0700

    Adds token creation and sending when user successfully logs in.

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -4,17 +4,19 @@ class AuthenticationController < ApplicationController
     @user = User.find_by username: params[:username]
 
     if !@user
-      render json: { message: 'Incorrect username' },
+      render json: { message: 'Incorrect username and password' },
         status: :unauthorized
     else
 
       if !@user.authenticate(params[:password])
-        render json: { message: 'Incorrect password' },
+        render json: { message: 'Incorrect username and password' },
           status: :unauthorized
       else
+        secret = 'only tell this to the bouncer'
+        payload = { user_id: @user.id }
+        token = JWT.encode(payload, secret)
 
-        
-
+        render json: { user: @user, token: token }
       end
 
     end
